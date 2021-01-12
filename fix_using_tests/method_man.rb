0fe65ec6266754ef982b39ee77ec48b8a2be94d1
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-12 18:39:38 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 # don't forget to add: require 'pry'
 
-def start_game(player1, player2)
+def start_game("Rocky", "Bullwinkle")
   puts "Hello #{player1} & #{player2}"
 end
-start_game("Rocky", "Bullwinkle")
+
+
 
 def play_game(player1, player2)
 puts "#{player1} is better than #{player2}"
