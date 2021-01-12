e893cfe000e9a7fe88f9811d4fe5c9c063959aa2
Tara Weathers <tara.n.weathers@gmail.com>
2021-01-12 18:36:55 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -3,9 +3,9 @@
 def start_game(player1, player2)
   puts "Hello #{player1} & #{player2}"
 end
-start_game("Chad", "Brad")
+start_game("Rocky", "Bullwinkle")
 
 def play_game(player1, player2)
 puts "#{player1} is better than #{player2}"
 end
-play_game("Chad", "Brad")
+play_game("Jay-Z", "Beyonce")
